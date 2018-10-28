fb2f711d290f4622e9dac5d997ecaaa2c7a59fd4
Hugh Rawlinson <hugh@spotify.com>
2018-10-28 17:57:51 +0100

    Updates

---
+++ precache-manifest.b01be8c4dbb944b1869eb7a598e275f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "038d650a5929c2024cbb",
+    "url": "/github-repo-organizer/static/js/runtime~main.038d650a.js"
+  },
+  {
+    "revision": "6dd8838843a4b52c401b",
+    "url": "/github-repo-organizer/static/js/main.6dd88388.chunk.js"
+  },
+  {
+    "revision": "1c021af7e822898f2745",
+    "url": "/github-repo-organizer/static/js/1.1c021af7.chunk.js"
+  },
+  {
+    "revision": "28e71997fe75eff96504310673330e96",
+    "url": "/github-repo-organizer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github-repo-organizer/precache-manifest.820a839c8bea55daa7f466c0691d6a6e.js"
+  "/github-repo-organizer/precache-manifest.b01be8c4dbb944b1869eb7a598e275f4.js"
 );
 
 workbox.clientsClaim();
