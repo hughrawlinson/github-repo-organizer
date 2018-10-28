1fc004fe4883459aed027ddce3d1664141d4416a
Hugh Rawlinson <hugh@spotify.com>
2018-10-28 13:07:11 +0100

    Updates

---
+++ precache-manifest.49524da538eb1e187c7cd7195f99a811.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "038d650a5929c2024cbb",
+    "url": "/github-repo-organizer/static/js/runtime~main.038d650a.js"
+  },
+  {
+    "revision": "034a9ff454880fc780de",
+    "url": "/github-repo-organizer/static/js/main.034a9ff4.chunk.js"
+  },
+  {
+    "revision": "35496709355efeab6da3",
+    "url": "/github-repo-organizer/static/js/1.35496709.chunk.js"
+  },
+  {
+    "revision": "2a726064a7b6d17f39b917fb6ffd053b",
+    "url": "/github-repo-organizer/index.html"
+  }
+];
