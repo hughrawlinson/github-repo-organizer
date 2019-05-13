97929f8a45e8bb90c7fc0bc667ad4c109cdc08f0
Hugh Rawlinson <hughr2005@gmail.com>
2019-05-12 21:31:56 -0400

    Updates

---
+++ precache-manifest.d94664a9f22be8592303cbdca0ce5805.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a6d3020bdb1160e0735b10c8d21adf",
+    "url": "/github-repo-organizer/index.html"
+  },
+  {
+    "revision": "b5a3cb2fde138f30b2b6",
+    "url": "/github-repo-organizer/static/js/2.cf7cc4d0.chunk.js"
+  },
+  {
+    "revision": "6a24b5ebb8ec95281dd3",
+    "url": "/github-repo-organizer/static/js/main.0a3e8f75.chunk.js"
+  },
+  {
+    "revision": "c60507a83c4eb753f4e5",
+    "url": "/github-repo-organizer/static/js/runtime~main.0e0f16a3.js"
+  }
+]);
