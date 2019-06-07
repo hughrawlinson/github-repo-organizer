6a715bd392b970f8da0b0e179b41a679ec8a1182
Hugh Rawlinson <hughr2005@gmail.com>
2019-06-06 20:16:58 -0400

    Use new MUI 'correct way' of handling spacing

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const styles = theme => ({
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing(3),
     height: '100vh',
     overflow: 'auto',
   },
