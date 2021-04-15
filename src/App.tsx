49a4e41c2b6d9c5b59e33461518ed8bb24487b02
Hugh Rawlinson <hughr2005@gmail.com>
2021-04-15 22:16:42 +0200

    switch app to functional component, pull out drawer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import React, { Component, ReactNode } from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { Theme, withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { connect } from "react-redux";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Repository } from "./sagas";
 var qs = require("query-string");
@@ -24,8 +17,14 @@ import Languages from "./components/Languages";
 import { GridState } from "./reducers";
 import { Dispatch } from "redux";
 import { RootState } from ".";
+import {
+  DrawerMenuToggleButton,
+  DrawerMenuWrapper,
+  DrawerMenu,
+} from "./components/DrawerMenu";
+import { ReactNode } from "react";
 
-const styles = (theme: any) => ({
+const styles = (theme: Theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -56,173 +55,101 @@ type AppProps = {
   setGridState: (v: GridState) => {};
 };
 
-type AppState = {
-  drawerOpen: boolean;
-};
-
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = {
-      drawerOpen: false,
-    };
-
-    this.ifLoggedOut = this.ifLoggedOut.bind(this);
-    this.ifLoggedIn = this.ifLoggedIn.bind(this);
-    this.ifRepositories = this.ifRepositories.bind(this);
-  }
-
-  toggleDrawer(value?: boolean) {
-    this.setState((state) => ({
-      ...state,
-      drawerOpen: value ? value : !state.drawerOpen,
-    }));
-  }
-
-  ifRepositories(child: ReactNode) {
-    if (this.props.repositories) {
+function App({
+  classes,
+  startLogIn,
+  refresh,
+  setGridState,
+  repositories,
+  loggedIn,
+}: AppProps) {
+  function ifLoggedOut(child: ReactNode) {
+    if (!loggedIn) {
       return child;
     }
     return null;
   }
 
-  ifLoggedOut(child: ReactNode) {
-    if (!this.props.loggedIn) {
+  function ifLoggedIn(child: ReactNode) {
+    if (loggedIn) {
       return child;
     }
     return null;
   }
 
-  ifLoggedIn(child: ReactNode) {
-    if (this.props.loggedIn) {
-      return child;
-    }
-    return null;
-  }
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Router>
-          <div className="App">
-            <AppBar>
-              <Toolbar>
-                {this.ifLoggedIn(
-                  <IconButton
-                    className={classes.menuButton}
-                    color="inherit"
-                    aria-label="Menu"
-                    onClick={() => this.toggleDrawer()}
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                )}
-                <Typography
-                  variant="h6"
+  return (
+    <DrawerMenuWrapper>
+      <CssBaseline />
+      <Router>
+        <div className="App">
+          <AppBar>
+            <Toolbar>
+              {ifLoggedIn(
+                <DrawerMenuToggleButton
+                  className={classes.menuButton}
                   color="inherit"
-                  className={classes.grow}
-                >
-                  GitHub Repo Organizer
-                </Typography>
-                {this.ifLoggedOut(
-                  <Button onClick={this.props.startLogIn} color="inherit">
-                    Login
-                  </Button>
-                )}
-                {this.ifLoggedIn(
-                  <Button onClick={this.props.refresh} color="inherit">
-                    Refresh
-                  </Button>
-                )}
-              </Toolbar>
-            </AppBar>
-            <Drawer
-              open={this.state.drawerOpen}
-              onClose={() => this.toggleDrawer(false)}
-            >
-              <div
-                onClick={() => this.toggleDrawer(false)}
-                onKeyDown={() => this.toggleDrawer(false)}
-              >
-                <List>
-                  <ListItem button>
-                    <Link to={process.env.PUBLIC_URL + "/"}>
-                      <ListItemText primary="Repositories" />
-                    </Link>
-                  </ListItem>
-                  <ListItem button>
-                    <Link to={process.env.PUBLIC_URL + "/topics"}>
-                      <ListItemText primary="Topics" />
-                    </Link>
-                  </ListItem>
-                  <ListItem button>
-                    <Link to={process.env.PUBLIC_URL + "/licenses"}>
-                      <ListItemText primary="Licenses" />
-                    </Link>
-                  </ListItem>
-                  <ListItem button>
-                    <Link to={process.env.PUBLIC_URL + "/languages"}>
-                      <ListItemText primary="Languages" />
-                    </Link>
-                  </ListItem>
-                </List>
-              </div>
-            </Drawer>
-            <main className={classes.content}>
-              <div className={classes.appBarSpacer} />
-              {this.ifLoggedOut(<LogIn />)}
-              {this.ifLoggedIn(
-                <Switch>
-                  <Route
-                    exact
-                    path={process.env.PUBLIC_URL + "/"}
-                    component={(props: { location: Location }) => {
-                      const queryParams = qs.parse(props.location.search);
-                      if (queryParams.gridState) {
-                        this.props.setGridState(
-                          JSON.parse(queryParams.gridState)
-                        );
-                      }
-                      return (
-                        <Repos
-                          queryParams={queryParams}
-                          repositories={this.props.repositories || []}
-                        />
-                      );
-                    }}
-                  />
-                  <Route
-                    exact
-                    path={process.env.PUBLIC_URL + "/topics"}
-                    component={() => (
-                      <Topics repositories={this.props.repositories} />
-                    )}
-                  />
-                  <Route
-                    exact
-                    path={process.env.PUBLIC_URL + "/licenses"}
-                    component={() => (
-                      <Licenses repositories={this.props.repositories} />
-                    )}
-                  />
-                  <Route
-                    exact
-                    path={process.env.PUBLIC_URL + "/languages"}
-                    component={() => (
-                      <Languages repositories={this.props.repositories} />
-                    )}
-                  />
-                </Switch>
+                  aria-label="Menu"
+                />
               )}
-            </main>
-          </div>
-        </Router>
-      </React.Fragment>
-    );
-  }
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                GitHub Repo Organizer
+              </Typography>
+              {ifLoggedOut(
+                <Button onClick={startLogIn} color="inherit">
+                  Login
+                </Button>
+              )}
+              {ifLoggedIn(
+                <Button onClick={refresh} color="inherit">
+                  Refresh
+                </Button>
+              )}
+            </Toolbar>
+          </AppBar>
+          <DrawerMenu />
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            {ifLoggedOut(<LogIn />)}
+            {ifLoggedIn(
+              <Switch>
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/"}
+                  component={(props: { location: Location }) => {
+                    const queryParams = qs.parse(props.location.search);
+                    if (queryParams.gridState) {
+                      setGridState(JSON.parse(queryParams.gridState));
+                    }
+                    return (
+                      <Repos
+                        queryParams={queryParams}
+                        repositories={repositories || []}
+                      />
+                    );
+                  }}
+                />
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/topics"}
+                  component={() => <Topics repositories={repositories} />}
+                />
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/licenses"}
+                  component={() => <Licenses repositories={repositories} />}
+                />
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/languages"}
+                  component={() => <Languages repositories={repositories} />}
+                />
+              </Switch>
+            )}
+          </main>
+        </div>
+      </Router>
+    </DrawerMenuWrapper>
+  );
 }
 
 const mapStateToProps = (state: RootState) => ({
@@ -246,8 +173,3 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 const stylist = withStyles(styles);
 
 export default stylist(connector(App));
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(withStyles(styles)(App));
---
+++ src/components/DrawerMenu.tsx
@@ -0,0 +1,84 @@
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { Link } from "react-router-dom";
+
+const DrawerMenuContext = createContext({
+  drawerOpen: false,
+  toggleDrawer: (_?: boolean) => {},
+});
+
+type DrawerMenuWrapperProps = {
+  children: ReactNode;
+};
+
+export function DrawerMenuWrapper({ children }: DrawerMenuWrapperProps) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer(newState?: boolean) {
+    if (typeof newState === "undefined") {
+      setDrawerOpen(!drawerOpen);
+      return;
+    } else {
+      setDrawerOpen(newState);
+    }
+  }
+
+  return (
+    <DrawerMenuContext.Provider value={{ drawerOpen, toggleDrawer }}>
+      {children}
+    </DrawerMenuContext.Provider>
+  );
+}
+
+export function DrawerMenuToggleButton(
+  props: React.ComponentProps<typeof IconButton>
+) {
+  const { toggleDrawer } = useContext(DrawerMenuContext);
+  return (
+    <IconButton onClick={() => toggleDrawer()}>
+      <MenuIcon />
+    </IconButton>
+  );
+}
+
+export function DrawerMenu() {
+  const { drawerOpen, toggleDrawer } = useContext(DrawerMenuContext);
+  return (
+    <Drawer open={drawerOpen} onClose={() => toggleDrawer(false)}>
+      <div
+        onClick={() => toggleDrawer(false)}
+        onKeyDown={() => toggleDrawer(false)}
+      >
+        <List>
+          <ListItem button>
+            <Link to={process.env.PUBLIC_URL + "/"}>
+              <ListItemText primary="Repositories" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link to={process.env.PUBLIC_URL + "/topics"}>
+              <ListItemText primary="Topics" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link to={process.env.PUBLIC_URL + "/licenses"}>
+              <ListItemText primary="Licenses" />
+            </Link>
+          </ListItem>
+          <ListItem button>
+            <Link to={process.env.PUBLIC_URL + "/languages"}>
+              <ListItemText primary="Languages" />
+            </Link>
+          </ListItem>
+        </List>
+      </div>
+    </Drawer>
+  );
+}
