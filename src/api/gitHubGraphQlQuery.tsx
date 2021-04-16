0df6f4b94aaca9507f79e906400a1cd612b17d4b
Hugh Rawlinson <hughr2005@gmail.com>
2021-04-16 11:21:24 +0200

    remove column abstraction

--- src/api/gitHubGraphQlQuery.tsx
+++ src/api/gitHubGraphQlQuery.tsx
@@ -1,117 +1,67 @@
-import columns from "../columns";
-
 const query = (endCursor: string) => `query {
     viewer {
-      repositories (first:100${
-        endCursor ? ', after:"' + endCursor + '"' : ""
-      }) {
+      repositories (first:40${endCursor ? ', after:"' + endCursor + '"' : ""}) {
         pageInfo {
           endCursor
         }
         totalCount
         nodes {
           id
-          ${Object.values(columns).map(
-            (column) => `...${column.getColumnFragment().name}\n`
-          )}
-          ...stargazers
-          ...primaryLanguage
-          ...private
-          ...archived
-          ...issues
-          ...pullRequestCount
-          ...owner
-          ...nameWithOwner
-          ...url
-          ...fork
-          ...licenses
-          ...vulnerabilityAlerts
-          ...collaborators
-        }
-      }
-    }
-  }
-
-${Object.values(columns).map((column) => column.getColumnFragment().fragment)}
-
-fragment stargazers on Repository {
-  stargazers {
-    totalCount
-  }
-}
-
-fragment primaryLanguage on Repository {
-  primaryLanguage {
-    name
-  }
-}
-
-fragment private on Repository {
-  isPrivate
-}
-
-fragment archived on Repository {
-  isArchived
-}
-
-fragment issues on Repository {
-  issues (states: OPEN) {
-    totalCount
-  }
-}
-
-fragment pullRequestCount on Repository {
-  pullRequests (states: OPEN) {
-    totalCount
-  }
-}
-
-fragment owner on Repository {
-  owner {
-    login
-  }
-}
-
-fragment nameWithOwner on Repository {
-  nameWithOwner
-}
-
-fragment url on Repository {
-  url
-}
-
-fragment fork on Repository {
-  isFork
-}
-
-fragment licenses on Repository {
-  licenseInfo {
-    name
-    nickname
-  }
-}
-
-fragment vulnerabilityAlerts on Repository {
-  vulnerabilityAlerts (first:50){
-    nodes {
-      securityVulnerability {
-        package {
           name
-        }
-        advisory {
           description
-          summary
+          createdAt
+          stargazers {
+            totalCount
+          }
+          primaryLanguage {
+            name
+          }
+          isPrivate
+          isArchived
+          issues (states: OPEN) {
+            totalCount
+          }
+          pullRequests (states: OPEN) {
+            totalCount
+          }
+          owner {
+            login
+          }
+          nameWithOwner
+          url
+          isFork
+          licenseInfo {
+            name
+            nickname
+          }
+          vulnerabilityAlerts (first:50){
+            nodes {
+              securityVulnerability {
+                package {
+                  name
+                }
+                advisory {
+                  description
+                  summary
+                }
+              }
+            }
+          }
+          collaborators (first: 50){
+            nodes {
+              name
+              login
+            }
+          }
+        repositoryTopics(first:100) {
+          nodes {
+            topic {
+              id
+              name
+            }
+          }
         }
       }
-    }
-  }
-}
-
-fragment collaborators on Repository {
-  collaborators (first: 50){
-    nodes {
-      name
-      login
     }
   }
 }
